7fcc3dacf2d663ed415f9ba93c37509288c02976
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-04-08 00:41:45 +0800

    Allowed host to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['linawa-frontend.onrender.com'],
     host:'0.0.0.0',
     port: process.env.PORT || 5173,  // Use the PORT environment variable, fallback to 5173 for local development
     strictPort: true,  // Ensure that the port is strictly followed (don't fall back to a different port)
